2a03a96ae15fb8f11b0a99d5cbad4c70c3c2b75e
Siddikeshwer.reddy <96370546+Siddikeshwer@users.noreply.github.com>
2025-06-20 16:29:18 +0530

    Update CommandTest.cs

--- DN 4.0 week1 Exercises/week1_Design_Patterns_Principles/CommandPatternExample/CommandTest.java
+++ DN 4.0 week1 Exercises/week1_Design_Patterns_Principles/CommandPatternExample/CommandTest.java
@@ -2,7 +2,95 @@ interface Command {
     void execute();
 }
 class Light {
-    public void on() {
+    public void on() {using System;
+
+// Command.cs
+public interface ICommand
+{
+    void Execute();
+}
+
+// Light.cs
+public class Light
+{
+    public void On()
+    {
+        Console.WriteLine("Light is ON");
+    }
+
+    public void Off()
+    {
+        Console.WriteLine("Light is OFF");
+    }
+}
+
+// LightOnCommand.cs
+public class LightOnCommand : ICommand
+{
+    private readonly Light _light;
+
+    public LightOnCommand(Light light)
+    {
+        _light = light ?? throw new ArgumentNullException(nameof(light));
+    }
+
+    public void Execute()
+    {
+        _light.On();
+    }
+}
+
+// LightOffCommand.cs
+public class LightOffCommand : ICommand
+{
+    private readonly Light _light;
+
+    public LightOffCommand(Light light)
+    {
+        _light = light ?? throw new ArgumentNullException(nameof(light));
+    }
+
+    public void Execute()
+    {
+        _light.Off();
+    }
+}
+
+// RemoteControl.cs
+public class RemoteControl
+{
+    private ICommand _command;
+
+    public void SetCommand(ICommand command)
+    {
+        _command = command ?? throw new ArgumentNullException(nameof(command));
+    }
+
+    public void PressButton()
+    {
+        _command.Execute();
+    }
+}
+
+// Program.cs
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Light light = new Light();
+
+        ICommand lightOn = new LightOnCommand(light);
+        ICommand lightOff = new LightOffCommand(light);
+
+        RemoteControl remote = new RemoteControl();
+
+        remote.SetCommand(lightOn);
+        remote.PressButton();
+
+        remote.SetCommand(lightOff);
+        remote.PressButton();
+    }
+}
         System.out.println("Light is ON");
     }
     
