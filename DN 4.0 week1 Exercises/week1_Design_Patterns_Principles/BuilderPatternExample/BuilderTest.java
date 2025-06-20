8372dcd34cc794f8d6e750984131d375beb2d90b
Siddikeshwer.reddy <96370546+Siddikeshwer@users.noreply.github.com>
2025-06-20 16:28:00 +0530

    Update BuilderTest.cs

--- DN 4.0 week1 Exercises/week1_Design_Patterns_Principles/BuilderPatternExample/BuilderTest.java
+++ DN 4.0 week1 Exercises/week1_Design_Patterns_Principles/BuilderPatternExample/BuilderTest.java
@@ -1,62 +1,76 @@
-class Computer {
-    private String CPU;
-    private String RAM;
-    private String storage;
-    private String GPU;
-    
-    private Computer(Builder builder) {
-        this.CPU = builder.CPU;
-        this.RAM = builder.RAM;
-        this.storage = builder.storage;
-        this.GPU = builder.GPU;
+using System;
+
+// Computer.cs
+public class Computer
+{
+    private readonly string _cpu;
+    private readonly string _ram;
+    private readonly string _storage;
+    private readonly string _gpu;
+
+    private Computer(Builder builder)
+    {
+        _cpu = builder.Cpu;
+        _ram = builder.Ram;
+        _storage = builder.Storage;
+        _gpu = builder.Gpu;
     }
-    
-    public static class Builder {
-        private String CPU;
-        private String RAM;
-        private String storage;
-        private String GPU;
-        
-        public Builder setCPU(String CPU) {
-            this.CPU = CPU;
+
+    public class Builder
+    {
+        private string Cpu;
+        private string Ram;
+        private string Storage;
+        private string Gpu;
+
+        public Builder SetCpu(string cpu)
+        {
+            Cpu = cpu;
             return this;
         }
-        
-        public Builder setRAM(String RAM) {
-            this.RAM = RAM;
+
+        public Builder SetRam(string ram)
+        {
+            Ram = ram;
             return this;
         }
-        
-        public Builder setStorage(String storage) {
-            this.storage = storage;
+
+        public Builder SetStorage(string storage)
+        {
+            Storage = storage;
             return this;
         }
-        
-        public Builder setGPU(String GPU) {
-            this.GPU = GPU;
+
+        public Builder SetGpu(string gpu)
+        {
+            Gpu = gpu;
             return this;
         }
-        
-        public Computer build() {
+
+        public Computer Build()
+        {
             return new Computer(this);
         }
     }
-    
-    @Override
-    public String toString() {
-        return "Computer [CPU=" + CPU + ", RAM=" + RAM + ", storage=" + storage + ", GPU=" + GPU + "]";
+
+    public override string ToString()
+    {
+        return $"Computer [CPU={_cpu}, RAM={_ram}, Storage={_storage}, GPU={_gpu}]";
     }
 }
 
-public class BuilderTest {
-    public static void main(String[] args) {
+// Program.cs
+public class Program
+{
+    public static void Main(string[] args)
+    {
         Computer computer = new Computer.Builder()
-            .setCPU("Intel i7")
-            .setRAM("16GB")
-            .setStorage("1TB SSD")
-            .setGPU("NVIDIA GTX 3080")
-            .build();
-        
-        System.out.println(computer);
+            .SetCpu("Intel i7")
+            .SetRam("16GB")
+            .SetStorage("1TB SSD")
+            .SetGpu("NVIDIA GTX 3080")
+            .Build();
+
+        Console.WriteLine(computer);
     }
 }
