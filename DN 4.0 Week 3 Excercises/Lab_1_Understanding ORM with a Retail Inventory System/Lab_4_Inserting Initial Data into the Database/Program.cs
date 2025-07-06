de1e14cc670f177a6aa64f57204a8c6e7bedddbe
Siddikeshwer.reddy <96370546+Siddikeshwer@users.noreply.github.com>
2025-07-06 17:16:43 +0530

    Add files via upload

---
+++ DN 4.0 Week 3 Excercises/Lab_1_Understanding ORM with a Retail Inventory System/Lab_4_Inserting Initial Data into the Database/Program.cs
@@ -0,0 +1,24 @@
+﻿using RetailInventory.Models;
+
+var context = new AppDbContext();
+
+if (!context.Categories.Any())
+{
+    var electronics = new Category { Name = "Electronics" };
+    var groceries = new Category { Name = "Groceries" };
+
+    await context.Categories.AddRangeAsync(electronics, groceries);
+
+    var product1 = new Product { Name = "Laptop", Price = 75000, Category = electronics };
+    var product2 = new Product { Name = "Rice Bag", Price = 1200, Category = groceries };
+
+    await context.Products.AddRangeAsync(product1, product2);
+
+    await context.SaveChangesAsync();
+
+    Console.WriteLine(" Data inserted successfully.");
+}
+else
+{
+    Console.WriteLine(" Data already exists in the database.");
+}
