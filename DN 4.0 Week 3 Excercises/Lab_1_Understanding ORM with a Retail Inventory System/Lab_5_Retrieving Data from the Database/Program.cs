ace1c838a15bcb964ca34b1260380cda7d29b26d
Siddikeshwer.reddy <96370546+Siddikeshwer@users.noreply.github.com>
2025-07-06 17:17:02 +0530

    Add files via upload

---
+++ DN 4.0 Week 3 Excercises/Lab_1_Understanding ORM with a Retail Inventory System/Lab_5_Retrieving Data from the Database/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+var context = new AppDbContext();
+
+var products = await context.Products.Include(p => p.Category).ToListAsync();
+Console.WriteLine(" All Products:");
+foreach (var p in products)
+{
+    Console.WriteLine($"{p.Name} - ₹{p.Price} - Category: {p.Category?.Name}");
+}
+
+var product = await context.Products.FindAsync(1);
+Console.WriteLine($"\n Find by ID 1: {product?.Name}");
+
+var expensive = await context.Products.FirstOrDefaultAsync(p => p.Price > 50000);
+Console.WriteLine($"\n Expensive Product: {expensive?.Name}");
