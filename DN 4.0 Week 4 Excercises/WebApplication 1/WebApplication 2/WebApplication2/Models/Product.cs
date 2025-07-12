05282a0abeac1ba617e892a1e6e03e282dbf5c70
Siddikeshwer.reddy <96370546+Siddikeshwer@users.noreply.github.com>
2025-07-12 09:45:22 +0530

    Add files via upload

---
+++ DN 4.0 Week 4 Excercises/WebApplication 1/WebApplication 2/WebApplication2/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi_Handson_2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
